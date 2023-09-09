4ac572cf5d794636a24d4f427c562fb2f6de91ec
Lada Jakab <132440338+Lada-Montcorbier@users.noreply.github.com>
2023-09-09 17:21:54 +0200

    upload init
    base files upload

---
+++ script.js
@@ -0,0 +1,54 @@
+const toggleSwitch = document.querySelector('input[type="checkbox"]');
+const nav = document.getElementById('nav');
+const toggleIcon = document.getElementById('toggle-icon');
+const image1 = document.getElementById('image1');
+const image2 = document.getElementById('image2');
+const image3 = document.getElementById('image3');
+
+// Dark/Light Images
+function imageMode(color) {
+  image1.src = `undraw_appreciate_it_re_yc8h_${color}.svg`;
+  image2.src = `undraw_dog_c7i6_${color}.svg`;
+  image3.src = `undraw_woman_ffrd_${color}.svg`;
+}
+
+// Dark Mode Function
+function darkMode() {
+  toggleIcon.children[0].textContent = 'Dark Mode';
+  toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
+  imageMode('dark');
+}
+
+// Light Mode Function
+function lightMode() {
+  toggleIcon.children[0].textContent = 'Light Mode';
+  toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
+  imageMode('light');
+}
+
+// Switch Theme Dynamicaly
+function switchTheme(event) {
+  if (event.target.checked) {
+    document.documentElement.setAttribute('data-theme', 'dark');
+    localStorage.setItem('theme', 'dark');
+    darkMode();
+  } else {
+    document.documentElement.setAttribute('data-theme', 'light');
+    localStorage.setItem('theme', 'light');
+    lightMode();
+  }
+}
+
+// Event Listener
+toggleSwitch.addEventListener('change', switchTheme);
+
+// Checl Local Storrage For Theme
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme) {
+  document.documentElement.setAttribute('data-theme', currentTheme);
+
+  if (currentTheme === 'dark') {
+    toggleSwitch.checked = true;
+    darkMode();
+  }
+}
